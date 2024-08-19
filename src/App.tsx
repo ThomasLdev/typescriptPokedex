60d2df74ff7c84e2221758a6fa6a5b593abe80de
tlefebvre-tikamoon <tlefebvre@tikamoon.com>
2024-08-19 21:54:13 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from "flowbite-react";
+
+function App() {
+  return (
+    <>
+      <h1>Let's begin</h1>
+        <Button>Click me</Button>
+    </>
+  )
+}
+
+export default App
